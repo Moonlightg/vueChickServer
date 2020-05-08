0747ca30c53f8a83ba9f8a9d375fc669b2404c6b
guojiabao <g1079832313@163.com>
2020-05-08 22:46:27 +0800

    增加用户商品数据,修改获取用户商品接口

--- api/goods-api.js
+++ api/goods-api.js
@@ -1,4 +1,5 @@
-const Good = require('../models/goods')
+const Good = require('../models/goods');
+const userGood = require('../models/usergoods')
 
 // 获取默认商品列表
 exports.getGoods = (req, res) => {
@@ -15,13 +16,13 @@ exports.getGoods = (req, res) => {
 // 获取用户商品列表
 exports.getUserGoods = (req, res) => {
     console.log(req.query);
-    // Good.find({},(err,data) => {
-    //     if (err) {
-    //         res.send({'code': 0, 'msg': '查询失败', 'data': err});
-    //     } else {
-    //         console.log('查询商品成功'+data)
-    //         res.send({ 'code': 1, 'data': data });
-    //     }
-    // })
+    userGood.find({},(err,data) => {
+        if (err) {
+            res.send({'code': 0, 'msg': '查询失败', 'data': err});
+        } else {
+            console.log('查询商品成功1'+data)
+            res.send({ 'code': 1, 'data': data });
+        }
+    })
 }
 
--- models/usergoods.js
+++ models/usergoods.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose')
 
 var userGoodsSchema = new mongoose.Schema({
 	openId: String,             // 用户唯一标示,_id
-    userGoodsList: Array       // 用户商品列表
+    name: String,               // 商品名称
+    num: Number                // 库存数量
 })
 
-// goods 为指定的collections集合
+// usergoods 为指定的collections集合
 module.exports = mongoose.model('userGoods', userGoodsSchema, 'usergoods')
