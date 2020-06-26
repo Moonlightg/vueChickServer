0c6043d5e58fd49f58fd16f22993dda0abe64e31
guojiabao <g1079832313@163.com>
2020-06-26 21:52:12 +0800

    优化道具参数

--- models/goods.js
+++ models/goods.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose')
 var goodsSchema = new mongoose.Schema({
 	openId: String,             // 用户唯一标示,_id
     name: String,               // 商品名称
-    type: Number,               // 商品类型,1:食物,2:道具
+    type: Number,               // 商品类型,1:食物,2:道具(只能出售),3:道具(能出售与使用)
     price: Number,              // 单价
     eatTime: Number,            // 进食时间 (单位毫秒)
     exp: Number,                // 增加小鸡经验
