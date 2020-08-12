2952bcfcf52f8489427375a1d3e679e399974513
guojiabao <g1079832313@163.com>
2020-08-12 16:16:50 +0800

    :smirk:日志动态增加icon标识说明

--- models/log.js
+++ models/log.js
@@ -4,7 +4,7 @@ var logSchema = new mongoose.Schema({
 	openId: String,                 // 用户唯一标示,_id
 	logList: [						// 日志列表
 		{
-			log_type: Number,       // 日志类型 (0:注册账号,1:登陆,2:退出登陆)
+			log_type: Number,       // 日志类型 (0:注册账号,1:登陆,2:退出登陆,3:解锁商品,4:购买商品,5:领取任务奖励,6:投喂食物)
 			log_title: String,      // 日志描述
 			log_date: String        // 日志时间
 		}
