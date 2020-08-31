a4f5f16fb5c17c29c9f8cdccfba07dbfea572519
guojiabao <g1079832313@163.com>
2020-08-31 20:57:20 +0800

    edit api

--- api/study-api.js
+++ api/study-api.js
@@ -1,14 +1,15 @@
 const User = require('../models/user');
-const Study = require('../models/study');
-const Chick = require('../models/chick');
+const UserStudy = require('../models/userstudy');
 
 // 获取用户学科列表
 exports.getUserStudy = (req, res) => {
-    Study.find({openId: req.query.userId},(err,data) => {
+	console.log("测试题目");
+	console.log(req.query);
+    UserStudy.find({openId: req.query.userId},(err,data) => {
         if (err) {
             res.send({'code': 0, 'msg': '查询失败', 'data': err});
         } else {
-            console.log('查询商品成功1'+data)
+            console.log('查询成功'+data)
             res.send({ 'code': 1, 'data': data });
         }
     })
